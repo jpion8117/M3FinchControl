6cc0bd1924815574f655f463065a779ccb77fc53
jp8117 <jp8117@live.com>
2021-03-12 17:53:36 -0500

    started project

---
+++ M3FinchControl/userProgramming.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M3FinchControl
+{
+    class userProgramming
+    {
+        enum Commands
+        {
+            Foreward,
+            Back,
+            Right,
+            Left,
+            GetTemperature,
+            GetLightLevel,
+            SetLED1,
+            SetLED2,
+            SetLED3,
+            ResetLED,
+            Beep1,
+            Beep2,
+            Beep3,
+            WaitCustom,
+            Wait1,
+            Wait30
+        }
+    }
+}
